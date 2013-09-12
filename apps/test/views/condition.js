662fbbcbbdd85eaf23a9bc0ab6fa63810c2b948e
Seva Rybakov <codingfu@gmail.com>
2013-09-12 21:28:29 +0300

    Tabs for condition treatments Added some basic css

--- apps/test/views/condition.js
+++ apps/test/views/condition.js
@@ -1,10 +1,36 @@
 define(["hbs!templates/condition"], function (template) {
     "use strict";
     
-    var ConditionView = Backbone.View.extend({
+    var ConditionView = Backbone.View.extend({        
+        events: {
+            "click .tabs a": "onTabClick"
+        },
+        
         render: function() {
             this.$el.html(template(this.model.toJSON()));
+            this.cacheElements();
+            this.selectFirstTab();
             return this;
+        },
+        
+        cacheElements: function() {
+            this.$tabs = this.$(".tabs li");
+            this.$tabContents = this.$('.tab-content');
+        },
+        
+        selectFirstTab: function() {
+            this.$tabs.find("a").eq(0).click();
+        },
+        
+        // Event handlers
+        onTabClick: function (e) {
+            this.$tabs.removeClass('active');
+            $(e.target).parent().addClass('active');
+            
+            var href = $(e.target).attr('href');
+            this.$tabContents.hide();
+            this.$(href).show();
+            return false;
         }
     });
     
